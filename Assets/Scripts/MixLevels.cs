9cf07490f5ffd5188a1df9dce4f81c8971b99a0d
jacobcheatley <jacobcheatley@gmail.com>
2016-04-17 18:19:07 +1200

    Volume mixer and controls

---
+++ Assets/Scripts/MixLevels.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixLevels : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void SetAnnouncerVolume(float level)
+    {
+        audioMixer.SetFloat("AnnouncerVolume", Mathf.Log10(level <= 0 ? 0.001f : level) * 40f);
+    }
+
+    public void SetMasterVolume(float level)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(level <= 0 ? 0.001f : level) * 40f);
+    }
+
+    public void SetMusicVolume(float level)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(level <= 0 ? 0.001f : level) * 40f);
+    }
+
+    public void SetSFXVolume(float level)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(level <= 0 ? 0.001f : level) * 40f);
+    }
+}
---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class PauseManager : MonoBehaviour
+{
+    private Canvas canvas;
+
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+
+	void Update()
+	{
+	    if (Input.GetKeyDown(KeyCode.Escape))
+	    {
+	        canvas.enabled = !canvas.enabled;
+	        Pause();
+	    }
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    }
+}
